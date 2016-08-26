10db26f6fe8757dff47e9f34a9e787f1412b927b
kk-22 <kk22job@gmail.com>
2016-08-26 19:59:09 +0900

    Create project

---
+++ app/src/main/java/jp/co/my/myplatform/myplatform/PLMainActivity.java
@@ -0,0 +1,13 @@
+package jp.co.my.myplatform.myplatform;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PLMainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
