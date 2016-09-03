112f95d56e89e315237f5729d0d12d1c253c370a
kk-22 <kk22job@gmail.com>
2016-09-04 00:51:44 +0900

    Load image on background

--- app/src/main/java/jp/co/my/myplatform/service/explorer/PLExplorerRecyclerAdapter.java
+++ app/src/main/java/jp/co/my/myplatform/service/explorer/PLExplorerRecyclerAdapter.java
@@ -1,7 +1,9 @@
 package jp.co.my.myplatform.service.explorer;
 
 import android.content.Context;
-import android.net.Uri;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,14 +12,18 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.List;
 
+import jp.co.my.common.util.MYLogUtil;
 import jp.co.my.common.util.MYStringUtil;
 import jp.co.my.myplatform.R;
 
 public class PLExplorerRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
 	private Context mContext;
+	private Handler mHandler;
+
 	private LayoutInflater mInflater;
 	private List<File> mFileList;
 	private PLOnClickFileListener mListener;
@@ -26,6 +32,7 @@ public class PLExplorerRecyclerAdapter extends RecyclerView.Adapter<RecyclerView
 		mInflater = LayoutInflater.from(context);
 		mContext = context;
 		mListener = listener;
+		mHandler = new Handler();
 	}
 
 	@Override
@@ -37,7 +44,7 @@ public class PLExplorerRecyclerAdapter extends RecyclerView.Adapter<RecyclerView
 	public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
 		PLImageViewHolder customHolder = (PLImageViewHolder) viewHolder;
 		final File file = mFileList.get(position);
-		customHolder.setFile(file);
+		updateHolderByFile(customHolder, file);
 		viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -51,43 +58,73 @@ public class PLExplorerRecyclerAdapter extends RecyclerView.Adapter<RecyclerView
 		return mFileList.size();
 	}
 
-	private static class PLImageViewHolder extends RecyclerView.ViewHolder {
-
-		private ImageView mImageView;
-		private TextView mTextView;
-
-		public PLImageViewHolder(View itemView) {
-			super(itemView);
-			mImageView = (ImageView) itemView.findViewById(R.id.icon_image);
-			mTextView = (TextView) itemView.findViewById(R.id.image_title);
+	public void updateHolderByFile(PLImageViewHolder holder, final File file) {
+		holder.mFile = file;
+		String fileName = file.getName();
+		String extension = MYStringUtil.getSuffix(fileName);
+		if (extension != null && (extension.equals("png") || extension.equals("jpg"))) {
+			loadImageOnBackground(holder, file);
+			holder.mTextView.setVisibility(View.GONE);
+			return;
 		}
 
-		public void setFile(final File file) {
-			String fileName = file.getName();
-			String extension = MYStringUtil.getSuffix(fileName);
-			if (extension != null && (extension.equals("png") || extension.equals("jpg"))) {
-				Uri uri = Uri.fromFile(file);
-				mImageView.setImageURI(uri);
-				mTextView.setVisibility(View.GONE);
-				return;
-			}
-
-			mTextView.setVisibility(View.VISIBLE);
-			if (file.isDirectory()) {
-				mImageView.setImageResource(R.drawable.directory);
-				mTextView.setText(fileName);
-				return;
-			} else {
-				mImageView.setImageResource(R.drawable.file);
-			}
+		holder.mTextView.setVisibility(View.VISIBLE);
+		if (file.isDirectory()) {
+			holder.mImageView.setImageResource(R.drawable.directory);
+			holder.mTextView.setText(fileName);
+			return;
+		} else {
+			holder.mImageView.setImageResource(R.drawable.file);
 		}
+	}
+
+	private void loadImageOnBackground(final PLImageViewHolder holder, final File file) {
+		holder.mImageView.setImageResource(R.drawable.file);
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					FileInputStream stream = new FileInputStream(file);
+					Bitmap bitmap = BitmapFactory.decodeStream(stream);
+					setImageOnMainThread(holder, file, bitmap);
+				} catch (Exception e) {
+					MYLogUtil.showExceptionToast(e);
+				}
+			}
+		}).start();
+	}
+
+	private void setImageOnMainThread(final PLImageViewHolder holder, final File file, final Bitmap bitmap) {
+		if (!holder.mFile.equals(file)) {
+			return;
+		}
+		mHandler.post(new Runnable() {
+			@Override
+			public void run() {
+				if (holder.mFile.equals(file)) {
+					holder.mImageView.setImageBitmap(bitmap);
+				}
+			}
+		});
+	}
+
+	public void setFileList(List<File> fileList) {
+		mFileList = fileList;
 	}
 
 	public interface PLOnClickFileListener {
 		void onClickFile(File file);
 	}
 
-	public void setFileList(List<File> fileList) {
-		mFileList = fileList;
+	private static class PLImageViewHolder extends RecyclerView.ViewHolder {
+		 File mFile;
+		ImageView mImageView;
+		TextView mTextView;
+
+		public PLImageViewHolder(View itemView) {
+			super(itemView);
+			mImageView = (ImageView) itemView.findViewById(R.id.icon_image);
+			mTextView = (TextView) itemView.findViewById(R.id.image_title);
+		}
 	}
 }
