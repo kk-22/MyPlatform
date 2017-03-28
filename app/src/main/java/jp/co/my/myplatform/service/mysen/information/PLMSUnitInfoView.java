ea294a1a376907cb5d740fb39b835a52c459fdaa
kk-22 <kk22job@gmail.com>
2017-03-28 20:34:25 +0900

    Set text color to parameter of UnitInfo

--- app/src/main/java/jp/co/my/myplatform/service/mysen/information/PLMSUnitInfoView.java
+++ app/src/main/java/jp/co/my/myplatform/service/mysen/information/PLMSUnitInfoView.java
@@ -1,6 +1,7 @@
 package jp.co.my.myplatform.service.mysen.information;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,11 +17,7 @@ import jp.co.my.myplatform.service.mysen.unit.PLMSSkillData;
 import jp.co.my.myplatform.service.mysen.unit.PLMSSkillModel;
 import jp.co.my.myplatform.service.popover.PLTextViewPopover;
 
-import static jp.co.my.myplatform.service.mysen.PLMSUnitData.PARAMETER_ATTACK;
-import static jp.co.my.myplatform.service.mysen.PLMSUnitData.PARAMETER_DEFENSE;
-import static jp.co.my.myplatform.service.mysen.PLMSUnitData.PARAMETER_MAGIC_DEFENSE;
 import static jp.co.my.myplatform.service.mysen.PLMSUnitData.PARAMETER_NUMBER;
-import static jp.co.my.myplatform.service.mysen.PLMSUnitData.PARAMETER_SPEED;
 import static jp.co.my.myplatform.service.mysen.PLMSUnitData.SKILL_NUMBER;
 
 
@@ -29,7 +26,7 @@ public class PLMSUnitInfoView extends LinearLayout implements View.OnClickListen
 	private TextView mLeftNameTextView;
 	private TextView mCurrentHPTextView;
 	private TextView mMaxHPTextView;
-	private TextView[] mParamTextViews;
+	private MYArrayList<TextView> mParamTextViewArray;
 	private MYArrayList<TextView> mSkillTextViewArray;
 
 	private PLMSUnitView mUnitView;
@@ -38,14 +35,18 @@ public class PLMSUnitInfoView extends LinearLayout implements View.OnClickListen
 		super(context, attrs, defStyle);
 		LayoutInflater.from(context).inflate(R.layout.mysen_unit_info, this);
 
-		mParamTextViews = new TextView[PARAMETER_NUMBER];
 		mLeftNameTextView = (TextView) findViewById(R.id.name_text);
 		mCurrentHPTextView = (TextView) findViewById(R.id.current_hp_text);
 		mMaxHPTextView = (TextView) findViewById(R.id.result_hp_text);
-		mParamTextViews[PARAMETER_ATTACK] = (TextView) findViewById(R.id.attack_text);
-		mParamTextViews[PARAMETER_SPEED] = (TextView) findViewById(R.id.speed_text);
-		mParamTextViews[PARAMETER_DEFENSE] = (TextView) findViewById(R.id.defense_text);
-		mParamTextViews[PARAMETER_MAGIC_DEFENSE] = (TextView) findViewById(R.id.magic_defense_text);
+
+		mParamTextViewArray = new MYArrayList<>(PARAMETER_NUMBER);
+		mParamTextViewArray.add((TextView) findViewById(R.id.attack_text));
+		mParamTextViewArray.add((TextView) findViewById(R.id.speed_text));
+		mParamTextViewArray.add((TextView) findViewById(R.id.defense_text));
+		mParamTextViewArray.add((TextView) findViewById(R.id.magic_defense_text));
+		for (TextView textView : mParamTextViewArray) {
+			textView.setOnClickListener(this);
+		}
 
 		mSkillTextViewArray = new MYArrayList<>(SKILL_NUMBER);
 		mSkillTextViewArray.add((TextView) findViewById(R.id.weapon_text));
@@ -54,7 +55,6 @@ public class PLMSUnitInfoView extends LinearLayout implements View.OnClickListen
 		mSkillTextViewArray.add((TextView) findViewById(R.id.passive_a_text));
 		mSkillTextViewArray.add((TextView) findViewById(R.id.passive_b_text));
 		mSkillTextViewArray.add((TextView) findViewById(R.id.passive_c_text));
-
 		for (TextView textView : mSkillTextViewArray) {
 			textView.setOnClickListener(this);
 		}
@@ -76,7 +76,18 @@ public class PLMSUnitInfoView extends LinearLayout implements View.OnClickListen
 		setIntToText(unitData.getCurrentHP(), mCurrentHPTextView);
 		setIntToText(unitData.getMaxHP(), mMaxHPTextView);
 		for (int i = 0; i < PARAMETER_NUMBER; i++) {
-			setIntToText(unitData.getCurrentParameterOfNo(i), mParamTextViews[i]);
+			TextView textView = mParamTextViewArray.get(i);
+			setIntToText(unitData.getCurrentParameterOfNo(i), textView);
+
+			int base = unitData.getBaseParameterOfNo(i);
+			int current = unitData.getCurrentParameterOfNo(i);
+			if (base == current) {
+				textView.setTextColor(Color.WHITE);
+			} else if (base < current) {
+				textView.setTextColor(Color.BLUE);
+			} else {
+				textView.setTextColor(Color.RED);
+			}
 		}
 
 		for (int i = 0; i < SKILL_NUMBER; i++) {
@@ -97,17 +108,32 @@ public class PLMSUnitInfoView extends LinearLayout implements View.OnClickListen
 	@Override
 	public void onClick(View v) {
 		TextView textView = (TextView) v;
-		int index = mSkillTextViewArray.indexOf(textView);
-		PLMSSkillData skillData = mUnitView.getUnitData().getSkillOfNo(index);
-		if (skillData == null) {
-			return;
+		String message;
+		int paramIndex = mParamTextViewArray.indexOf(textView);
+		if (paramIndex > -1) {
+			PLMSUnitData unitData = mUnitView.getUnitData();
+			int base = unitData.getBaseParameterOfNo(paramIndex);
+			int buff = unitData.getBuffParameterOfNo(paramIndex);
+			StringBuilder builder = new StringBuilder();
+			builder.append("基本値");
+			builder.append(base);
+			if (buff != 0) {
+				builder.append("　強化+");
+				builder.append(buff);
+			}
+			message = new String(builder);
+		} else {
+			int skillIndex = mSkillTextViewArray.indexOf(textView);
+			PLMSSkillData skillData = mUnitView.getUnitData().getSkillOfNo(skillIndex);
+			if (skillData == null) {
+				return;
+			}
+			PLMSSkillModel skillModel = skillData.getSkillModel();
+			if (skillModel == null) {
+				return;
+			}
+			message = skillModel.getDescription();
 		}
-		PLMSSkillModel skillModel = skillData.getSkillModel();
-		if (skillModel == null) {
-			return;
-		}
-
-		String description = skillModel.getDescription();
-		new PLTextViewPopover(description).showPopover(new PLRelativeLayoutController(v));
+		new PLTextViewPopover(message).showPopover(new PLRelativeLayoutController(v));
 	}
 }
