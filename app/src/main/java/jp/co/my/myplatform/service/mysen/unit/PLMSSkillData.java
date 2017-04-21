41140bf3357694ea616c25805286beded34a5be7
kk-22 <kk22job@gmail.com>
2017-04-21 23:58:52 +0900

    Move enum position in SkillData

--- app/src/main/java/jp/co/my/myplatform/service/mysen/unit/PLMSSkillData.java
+++ app/src/main/java/jp/co/my/myplatform/service/mysen/unit/PLMSSkillData.java
@@ -23,6 +23,125 @@ import jp.co.my.myplatform.service.mysen.battle.PLMSSupportUnit;
 
 public class PLMSSkillData {
 
+
+	private static final int SKILL_ATTACK = 0x0001;
+	private static final int SKILL_SPEED = 0x0002;
+	private static final int SKILL_DEFENSE = 0x0004;
+	private static final int SKILL_MAGIC_DEFENSE = 0x0008;
+	private static final int SKILL_HP = 0x0010;
+
+	public enum EffectType {
+		NONE(0),
+		ONE_TURN_BUFF(1), BATTLE_BUFF(2), ALL_TURN_BATTLE_BUFF(3),
+		FLUCTUATE_HP(11), HEAL_ME_TOO(12),
+		CHASE_ATTACK_IF_HAS_COUNTER(21), CHASE_ATTACK(22), CONTINUOUSLY_CHASE_ATTACK(23),
+		DISABLE_CHASE_ATTACK(26), KILL_WEAPON(27), PREEMPTIVE_ATTACK(28), KAZANAGI(29),
+		SLIP_MOVE(31),WARP_TO_TEAM(32), WARP_TO_TEAM_OF_LESS_HP(33), BLOCK_ENEMY_MOVE(36),
+		THREE_WAY_INTENSIFICATION(41), ADD_ADVANTAGE_COLOR(42), ALL_RANGE_COUNTER(43), PROTECT_WEAKNESS_ATTACK(44),
+		FLUCTUATE_SECRET_DAMAGE(46), CONTINUOUSLY_ATTACK(47),
+		CHANGE_POSITION(51), AGAIN_ACTION(56),
+		SAINTS(61), REVERSE(62), DEDICATION(63), MUTUAL_ASSISTANCE(64),
+		IKKATU(71), PANIC(72), CHANGE_MOVE_POWER(73);
+
+		final int id;
+		EffectType(final int id) {
+			this.id = id;
+		}
+		public int getInt() {
+			return this.id;
+		}
+
+		static private EffectType getType(int no) {
+			EffectType[] types = EffectType.values();
+			for (EffectType type : types) {
+				if (type.getInt() == no) {
+					return type;
+				}
+			}
+			MYLogUtil.showErrorToast("未実装の EffectType=" +no);
+			return ONE_TURN_BUFF;
+		}
+	}
+
+	private enum TimingType {
+		NONE(0),
+		START_TURN(1),
+		START_BATTLE(11), ATTACK_TO_ENEMY(12), ATTACK_TO_ME(13), START_TEAM_BATTLE(14),
+		FINISH_BATTLE(16), FINISH_ATTACK_BATTLE(17), FINISH_DEFENCE_BATTLE(18),
+		MY_MOVEMENT(21), ENEMY_MOVEMENT(22),
+		SUPPORT_TIMING(31), HEAL_BY_STAFF(32);
+
+		final int id;
+		TimingType(final int id) {
+			this.id = id;
+		}
+		public int getInt() {
+			return this.id;
+		}
+
+		static private TimingType getType(int no) {
+			TimingType[] types = TimingType.values();
+			for (TimingType type : types) {
+				if (type.getInt() == no) {
+					return type;
+				}
+			}
+			MYLogUtil.showErrorToast("未実装の TimingType=" +no);
+			return START_TURN;
+		}
+	}
+
+	private enum RequirementType {
+		NONE(0),
+		LESS_THAN_MY_HP(1), GREATER_THAN_MY_HP(2),
+		NUMBER_OF_TURN(11);
+
+		final int id;
+		RequirementType(final int id) {
+			this.id = id;
+		}
+		public int getInt() {
+			return this.id;
+		}
+
+		static private RequirementType getType(int no) {
+			RequirementType[] types = RequirementType.values();
+			for (RequirementType type : types) {
+				if (type.getInt() == no) {
+					return type;
+				}
+			}
+			MYLogUtil.showErrorToast("未実装の RequirementType=" +no);
+			return NONE;
+		}
+	}
+
+	private enum TargetType {
+		NONE(0),
+		SELF(1), TEAM_IN_RANGE(2), TEAM_OTHER_THAN_ME(3),
+		TARGET(6), TARGET_AND_SELF(7),
+		ENEMY(11), ENEMY_IN_ENEMY_RANGE(12), ENEMY_IN_MY_RANGE(13);
+
+		final int id;
+		TargetType(final int id) {
+			this.id = id;
+		}
+		public int getInt() {
+			return this.id;
+		}
+
+		static private TargetType getType(int no) {
+			TargetType[] types = TargetType.values();
+			for (TargetType type : types) {
+				if (type.getInt() == no) {
+					return type;
+				}
+			}
+			MYLogUtil.showErrorToast("未実装の TargetType=" +no);
+			return NONE;
+		}
+	}
+
 	private PLMSArgument mArgument;
 	private PLMSSkillModel mSkillModel;
 	private TimingType mTimingType;
@@ -623,123 +742,5 @@ public class PLMSSkillData {
 
 	public boolean isAvailable() {
 		return (mSkillModel != null);
-	}
-
-	private static final int SKILL_ATTACK = 0x0001;
-	private static final int SKILL_SPEED = 0x0002;
-	private static final int SKILL_DEFENSE = 0x0004;
-	private static final int SKILL_MAGIC_DEFENSE = 0x0008;
-	private static final int SKILL_HP = 0x0010;
-
-	private enum TimingType {
-		NONE(0),
-		START_TURN(1),
-		START_BATTLE(11), ATTACK_TO_ENEMY(12), ATTACK_TO_ME(13), START_TEAM_BATTLE(14),
-		FINISH_BATTLE(16), FINISH_ATTACK_BATTLE(17), FINISH_DEFENCE_BATTLE(18),
-		MY_MOVEMENT(21), ENEMY_MOVEMENT(22),
-		SUPPORT_TIMING(31), HEAL_BY_STAFF(32);
-
-		final int id;
-		TimingType(final int id) {
-			this.id = id;
-		}
-		public int getInt() {
-			return this.id;
-		}
-
-		static private TimingType getType(int no) {
-			TimingType[] types = TimingType.values();
-			for (TimingType type : types) {
-				if (type.getInt() == no) {
-					return type;
-				}
-			}
-			MYLogUtil.showErrorToast("未実装の TimingType=" +no);
-			return START_TURN;
-		}
-	}
-
-	private enum RequirementType {
-		NONE(0),
-		LESS_THAN_MY_HP(1), GREATER_THAN_MY_HP(2),
-		NUMBER_OF_TURN(11);
-
-		final int id;
-		RequirementType(final int id) {
-			this.id = id;
-		}
-		public int getInt() {
-			return this.id;
-		}
-
-		static private RequirementType getType(int no) {
-			RequirementType[] types = RequirementType.values();
-			for (RequirementType type : types) {
-				if (type.getInt() == no) {
-					return type;
-				}
-			}
-			MYLogUtil.showErrorToast("未実装の RequirementType=" +no);
-			return NONE;
-		}
-	}
-
-	private enum TargetType {
-		NONE(0),
-		SELF(1), TEAM_IN_RANGE(2), TEAM_OTHER_THAN_ME(3),
-		TARGET(6), TARGET_AND_SELF(7),
-		ENEMY(11), ENEMY_IN_ENEMY_RANGE(12), ENEMY_IN_MY_RANGE(13);
-
-		final int id;
-		TargetType(final int id) {
-			this.id = id;
-		}
-		public int getInt() {
-			return this.id;
-		}
-
-		static private TargetType getType(int no) {
-			TargetType[] types = TargetType.values();
-			for (TargetType type : types) {
-				if (type.getInt() == no) {
-					return type;
-				}
-			}
-			MYLogUtil.showErrorToast("未実装の TargetType=" +no);
-			return NONE;
-		}
-	}
-
-	public enum EffectType {
-		NONE(0),
-		ONE_TURN_BUFF(1), BATTLE_BUFF(2), ALL_TURN_BATTLE_BUFF(3),
-		FLUCTUATE_HP(11), HEAL_ME_TOO(12),
-		CHASE_ATTACK_IF_HAS_COUNTER(21), CHASE_ATTACK(22), CONTINUOUSLY_CHASE_ATTACK(23),
-		DISABLE_CHASE_ATTACK(26), KILL_WEAPON(27), PREEMPTIVE_ATTACK(28), KAZANAGI(29),
-		SLIP_MOVE(31),WARP_TO_TEAM(32), WARP_TO_TEAM_OF_LESS_HP(33), BLOCK_ENEMY_MOVE(36),
-		THREE_WAY_INTENSIFICATION(41), ADD_ADVANTAGE_COLOR(42), ALL_RANGE_COUNTER(43), PROTECT_WEAKNESS_ATTACK(44),
-		FLUCTUATE_SECRET_DAMAGE(46), CONTINUOUSLY_ATTACK(47),
-		CHANGE_POSITION(51), AGAIN_ACTION(56),
-		SAINTS(61), REVERSE(62), DEDICATION(63), MUTUAL_ASSISTANCE(64),
-		IKKATU(71), PANIC(72), CHANGE_MOVE_POWER(73);
-
-		final int id;
-		EffectType(final int id) {
-			this.id = id;
-		}
-		public int getInt() {
-			return this.id;
-		}
-
-		static private EffectType getType(int no) {
-			EffectType[] types = EffectType.values();
-			for (EffectType type : types) {
-				if (type.getInt() == no) {
-					return type;
-				}
-			}
-			MYLogUtil.showErrorToast("未実装の EffectType=" +no);
-			return ONE_TURN_BUFF;
-		}
 	}
 }
