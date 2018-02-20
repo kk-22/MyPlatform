a3aee0ce69383507373e9bd9e99dcfe40cfa75c7
kk-22 <kk22job@gmail.com>
2018-02-20 20:46:12 +0900

    Sort files by last modified in explorer

--- app/src/main/java/jp/co/my/myplatform/explorer/PLExplorerContent.java
+++ app/src/main/java/jp/co/my/myplatform/explorer/PLExplorerContent.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 import jp.co.my.common.util.MYLogUtil;
@@ -34,8 +35,8 @@ public class PLExplorerContent extends PLContentView implements PLExplorerRecycl
 	public PLExplorerContent() {
 		super();
 		LayoutInflater.from(getContext()).inflate(R.layout.content_explorer, this);
-		mPathText = (TextView) findViewById(R.id.path_text);
-		mRecyclerView = (RecyclerView) findViewById(R.id.recycler);
+		mPathText = findViewById(R.id.path_text);
+		mRecyclerView = findViewById(R.id.recycler);
 		mAdapter = new PLExplorerRecyclerAdapter(getContext(), this);
 
 		SharedPreferences pref = MYLogUtil.getPreference();
@@ -100,6 +101,13 @@ public class PLExplorerContent extends PLContentView implements PLExplorerRecycl
 	private void loadDirectory(File file) {
 		String path = file.getPath();
 		File[] allFiles = new File(path).listFiles();
+		Arrays.sort(allFiles, new Comparator<File>() {
+			@Override
+			public int compare(File f1, File f2) {
+				// 日付順
+				return (int)(f1.lastModified() - f2.lastModified());
+			}
+		});
 		List<File> fileList = Arrays.asList(allFiles);
 		if (fileList.size() == 0) {
 			MYLogUtil.showToast("ファイルなし");
